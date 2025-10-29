a3d235be00751f8a36ecea960bb85fb6c41b3236
Jack Bawer <jackb2507@gmail.com>
2025-10-29 15:39:06 +0100

    xresources failed me I failed me

--- config.def.h
+++ config.def.h
@@ -100,7 +100,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.5;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -126,7 +126,8 @@ static const char *colorname[] = {
 	"#555555", /* 257 -> rev cursor*/
 	"#282828", /* 258 -> bg */
 	"#ebdbb2", /* 259 -> fg */
-};
+}; 
+
 
 /*
  * Default colors (colorname index)
